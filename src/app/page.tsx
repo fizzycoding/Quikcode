d6a6faefe3454ec4022c0c425837757a8b88d7e9
fizzycoding <thunderxpubg37@gmail.com>
2025-08-15 14:35:40 +0530

    03 trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
-import { Button } from "@/components/ui/button";
-import React from "react";
+import { caller, getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import Client from "./client";
+import { Suspense } from "react";
 
 const page = async () => {
-  return <div className="font-bold"></div>;
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.kirito.queryOptions({ text: "Hello Server" })
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 
 export default page;
