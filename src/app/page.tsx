dc0f2081209eeadc0f4aba1ab483867898ae8a1c
fizzycoding <thunderxpubg37@gmail.com>
2025-08-15 16:17:18 +0530

    05:Ai-Jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Toaster } from "@/components/ui/sonner";
+import { useState } from "react";
+
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 const page = () => {
   const trpc = useTRPC();
+  const [value, setValue] = useState("");
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
       onSuccess: () => {
@@ -16,10 +20,16 @@ const page = () => {
     })
   );
   return (
-    <div>
+    <div className="">
+      <Input
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
       <Button
         disabled={invoke.isPending}
-        onClick={() => invoke.mutate({ text: "Ajju" })}
+        onClick={() => invoke.mutate({ text: value })}
       >
         Invoke Job
       </Button>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,25 @@
+import { openai, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "5s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const summarizer = createAgent({
+      name: "summarizer",
+      system:
+        "You are an expert next.js developer. you write redable, maintainable code you write simple next.js & react snippets",
+      model: openai({
+        model: "provider-6/gpt-4o",
+        baseUrl: "https://api.a4f.co/v1",
+      }),
+    });
+    const { output } = await summarizer.run(
+      `Write the following snipit: ${event.data.value}`
+    );
+    console.log(output);
+
+    return { success: "Ok", output };
   }
 );
