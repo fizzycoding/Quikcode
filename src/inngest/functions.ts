b0eba4332e12742e0ee5301effa9c2f9f9b8020c
fizzycoding <thunderxpubg37@gmail.com>
2025-08-16 10:46:44 +0530

    06: e2b-sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,16 @@
 import { openai, createAgent } from "@inngest/agent-kit";
-
+import { Sandbox } from "@e2b/code-interpreter";
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("quikcode-nextjs-test2");
+      return sandbox.sandboxId;
+    });
     const summarizer = createAgent({
       name: "summarizer",
       system:
@@ -15,11 +20,16 @@ export const helloWorld = inngest.createFunction(
         baseUrl: "https://api.a4f.co/v1",
       }),
     });
+
     const { output } = await summarizer.run(
       `Write the following snipit: ${event.data.value}`
     );
-    console.log(output);
 
-    return { success: "Ok", output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+    return { output, sandboxUrl };
   }
 );
