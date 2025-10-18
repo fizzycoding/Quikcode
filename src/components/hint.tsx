6cae9c7db36b0b408d524b650b67d147f4252d00
fizzycoding <thunderxpubg37@gmail.com>
2025-10-18 21:57:34 +0530

    12: Fragment preview

---
+++ src/components/hint.tsx
@@ -0,0 +1,33 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import React from "react";
+
+interface HintProps {
+  children: React.ReactNode;
+  text: string;
+  side?: "top" | "right" | "bottom" | "left";
+  align?: "start" | "center" | "end";
+}
+
+const Hint = ({
+  children,
+  text,
+  align = "center",
+  side = "top",
+}: HintProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent side={side} align={align}>
+          <p>{text}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+export default Hint;
