84628b4b9ffe5d8cad5f3fe5706bed4d96f77595
fizzycoding <thunderxpubg37@gmail.com>
2025-10-20 06:24:49 +0530

    Theme

--- src/modules/pojects/ui/components/message-card.tsx
+++ src/modules/pojects/ui/components/message-card.tsx
@@ -123,7 +123,7 @@ const FragmentCard = ({
       className={cn(
         "flex items-start text-start gap-2 border rounded-lg bg-muted w-fit p-3 hover:bg-secondary transition-colors",
         isActiveFragment &&
-          "bg-primary text-primary-foreground border-primary hover:bg-primary"
+          "bg-primary text-primary-foreground dark:text-white border-primary hover:bg-primary"
       )}
       onClick={() => onFragmentClick(fragment)}
     >
